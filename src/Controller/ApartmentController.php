a9212fc1b1a903c5409de3c163cfdb351ffe49ab
Jose Navarrete <janavarretecristino@gmail.com>
2018-10-23 12:17:56 +0200

    frontend added

--- src/Controller/ApartmentController.php
+++ src/Controller/ApartmentController.php
@@ -8,11 +8,15 @@ use App\DTO\FilterParametersDTO;
 use App\UseCase\GetApartments;
 use App\UseCase\GetApartmentsUseCase;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApartmentController extends Controller
 {
+    private const FILE_FORMAT = 'json_file';
+    private const FILE_NAME = 'apartments.json';
+
     /**
      * @var GetApartmentsUseCase
      */
@@ -23,14 +27,29 @@ class ApartmentController extends Controller
         $this->getApartmentsUseCase = $getApartmentsUseCase;
     }
 
-    public function getAction(Request $request): JsonResponse
+    public function getAction(Request $request): Response
     {
         $pageNumber = $request->query->getInt('page');
         $pageSize = $request->query->getInt('page_size');
         $orderBy = $request->query->get('orderBy');
+        $format = $request->query->get('format');
 
         $filterParameters = new FilterParametersDTO($orderBy, $pageNumber, $pageSize);
+        $apartments = $this->getApartmentsUseCase->execute($filterParameters);
 
-        return $this->json($this->getApartmentsUseCase->execute($filterParameters));
+        if (self::FILE_FORMAT === $format) {
+            $this->createFileFromJsonData(json_encode($apartments));
+
+            return $this->file(new File(self::FILE_NAME));
+        }
+
+        return $this->json($apartments);
+    }
+
+    private function createFileFromJsonData(string $jsonData): void
+    {
+        $file = fopen(self::FILE_NAME, 'w');
+        fwrite($file, $jsonData);
+        fclose($file);
     }
 }
